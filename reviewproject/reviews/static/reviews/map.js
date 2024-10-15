73715ee403f4081024f8992508f68d83971afaa0
10Fari10 <farihafa@buffalo.edu>
2024-10-14 23:21:31 -0400

    pins have add post button -needs to be fixed

--- reviewproject/reviews/static/reviews/map.js
+++ reviewproject/reviews/static/reviews/map.js
@@ -1,3 +1,4 @@
+const postButton='<button id="anotherPost">Add Post</button>';
 var map = L.map('map').setView([43.0001, -78.7865], 15)
 
 
@@ -6,9 +7,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 //pin function
 function x_marks_the_spot(x) {
-    var marker = L.marker([x.latlng.lat,x.latlng.lng]).addTo(map);
-    document.getElementById("addPost").style.display="block";   
-
+    var marker = L.marker([x.latlng.lat,x.latlng.lng]).addTo(map).bindPopup(postButton);
+    document.getElementById("addPost").style.display="block";  
+    marker.on("popupopen",extraPost); 
 }
 
 map.on('click', x_marks_the_spot);
+  //popDown fuction-click the 'x' for the post form to dissappear
+  
+  document.getElementById("closePop").addEventListener("click", popDown);
+  function popDown(){
+  document.getElementById("addPost").style.display="none";
+  }
+  //post through pin
+  function extraPost(){
+  document.getElementById("addPost").style.display="none";
+  }
