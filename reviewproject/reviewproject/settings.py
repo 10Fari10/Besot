7e920706977323a747e0192e89b38bdcfb46be91
oscar <oscarlilucky@gmail.com>
2024-11-27 14:05:48 -0500

    add hosts

--- reviewproject/reviewproject/settings.py
+++ reviewproject/reviewproject/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
-ALLOWED_HOSTS = ["localhost",'web']
+ALLOWED_HOSTS = ["localhost",'web',"137.184.156.82","billsoftrails.com"]
 
 
 # Application definition
