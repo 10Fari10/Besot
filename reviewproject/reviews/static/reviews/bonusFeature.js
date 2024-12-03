52dd88e46069eb9f64e8c192a5dffe3e120164aa
10Fari10 <farihafa@buffalo.edu>
2024-12-02 22:39:13 -0500

    Geolocation API setup

--- reviewproject/reviews/static/reviews/bonusFeature.js
+++ reviewproject/reviews/static/reviews/bonusFeature.js
@@ -1,5 +1,23 @@
+const holder=[];
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(store_n_pop);
+  }
+}
+
+function store_n_pop(point) {
+  
+  
+  holder.push(point.coords.latitude,point.coords.longitude);
+  document.getElementById("addLoco").style.display="block";
+
+ 
+  
+}
 
 var change=document.getElementById("val");
 var t_range=document.getElementById("time").oninput=function(){
   change.innerHTML=this.value;
 }
+
+getLocation();
