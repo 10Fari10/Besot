20c416ccf199474594f67419ef6a275231d2bbbb
oscar <oscarlilucky@gmail.com>
2024-12-08 23:00:25 -0500

    Made timer dependent on actual time

--- reviewproject/game/views.py
+++ reviewproject/game/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from django.http import HttpResponse
 from game.models import *
-import json
-import time
+
+from datetime import timezone,timedelta,datetime
 
 import logging
 from django.conf import settings
@@ -14,46 +13,22 @@ lvl = getattr(settings, 'LOG_LEVEL', logging.DEBUG)
 logging.basicConfig(format=fmt, level=lvl)
 logging.debug("Logging started on %s for %s" % (logging.root.name, logging.getLevelName(lvl)))
 
+
 def index(request):
     username = request.user.username
     if UserData.objects.filter(username=username).exists():
         user_data = UserData.objects.filter(username=username).first()
-        if user_data:
-            time_user = user_data.time
-            m = time_user // 600
-            s = (time_user % 600) // 10
-            c = time_user % 10
-            m = str(m)
-            s = str(s)
-            c = str(c)
-            if len(m) == 1:
-                m = "0" + m
-            if len(s) == 1:
-                s = "0" + s
-            if len(c) == 1:
-                c = "0" + c
-            time_final = m + ":" + s + ":" + c
-            user_data_list = UserData.objects.all().order_by('time')[:10]
+        if user_data and user_data.time!=None:
+            time_final = str.zfill(str(int((user_data.time) / 60)),2) +":"+ str.zfill(str(user_data.time % 60),2)
+            user_data_list = UserData.objects.all().exclude(time=None).order_by('time')[:10]
 
             leaderboard = []
             for u in user_data_list:
-
-                time_con = u.time
-                m = time_con // 600
-                s = (time_con % 600) // 10
-                c = time_con % 10
-                m = str(m)
-                s = str(s)
-                c = str(c)
-                if len(m) == 1:
-                    m = "0" + m
-                if len(s) == 1:
-                    s = "0" + s
-                if len(c) == 1:
-                    c = "0" + c
-                t_final = m + ":" + s + ":" + c
+                t_final= str.zfill(str(int((u.time) / 60)),2) +":"+ str.zfill(str(u.time % 60),2)
                 leaderboard.append((u.username, t_final))
-        return render(request, "game/completed_screen.html", {'time': time_final, 'leaderboard': leaderboard})
+            return render(request, "game/completed_screen.html", {'time': time_final, 'leaderboard': leaderboard})
+        else:
+            render(request, "game/index.html")
     return render(request, "game/index.html")
 
 #Changed my mind
@@ -61,16 +36,6 @@ def index(request):
 #Once solution is submitted, lobby name is used to lookup mapdata and compare solution
 #If verified, query is made to user table and all users with matching lobby are pulled, sorted by time, and sent
 
-#43.00235605877655 map.js:28:25
-# -78.77763672611523 map.js:29:25
-# 42.999468562433165 map.js:28:25
-# -78.79463196260592 map.js:29:25
-# 42.99849557114192 map.js:28:25
-# -78.80136997303279 map.js:29:25
-# 43.00367421851207 map.js:28:25
-# -78.81184178541595 map.js:29:25
-# 42.995545116259315 map.js:28:25
-# -78.77999717562783
 def room(request, room_name):
     mapSolutions = {"alpha":"r1,r2,r3,r4"}
     mapLayouts = {
@@ -95,55 +60,15 @@ def room(request, room_name):
              
              return render(request, "game/room.html", {'map_layout': map_layout, 'room_name': room_name})
 
-# def getLeaderboards(request):
-#     if not request.user.is_authenticated:
-#         return HttpResponse('Unauthorized', status=401)
-#     users = UserData.objects.filter(time__isnull=False).order_by('time')[:10]
-#     if len(users) < 10:
-#         users = UserData.objects.filter(time__isnull=False).order_by('time')
-#     if len(users) >0:
-#         return render(request, "game/room.html", {'leaderboard': users})
-#     else:
-#         return render(request, "game/index.html", {'message': 'No users found'})
-#
-#
-
 def completed_screen(request):
     username = request.user.username
     user_data = UserData.objects.filter(username=username).first()
     if user_data:
-        time_user = user_data.time
-        m = time_user // 600
-        s = (time_user % 600) // 10
-        c = time_user % 10
-        m = str(m)
-        s = str(s)
-        c = str(c)
-        if len(m) == 1:
-            m = "0" + m
-        if len(s) == 1:
-            s = "0" + s
-        if len(c) == 1:
-            c = "0" + c
-        time_final = m + ":" + s + ":" + c
-        user_data_list = UserData.objects.all().order_by('time')[:10]
+        time_final = str.zfill(str(int((user_data.time) / 60)),2) +":"+ str.zfill(str(user_data.time % 60),2)
+        user_data_list = UserData.objects.all().exclude(time=None).order_by('time')[:10]
 
         leaderboard = []
         for u in user_data_list:
-
-            time_con = u.time
-            m = time_con // 600
-            s = (time_con % 600) // 10
-            c = time_con % 10
-            m = str(m)
-            s = str(s)
-            c = str(c)
-            if len(m) == 1:
-                m = "0" + m
-            if len(s) == 1:
-                s = "0" + s
-            if len(c) == 1:
-                c = "0" + c
-            t_final = m + ":" + s + ":" + c
+            t_final = str.zfill(str(int((u.time) / 60)),2) +":"+ str.zfill(str(u.time % 60),2)
             leaderboard.append((u.username, t_final))
     return render(request, "game/completed_screen.html", {'time': time_final,'leaderboard': leaderboard})
